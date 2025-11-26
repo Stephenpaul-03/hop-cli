46bd9ac81924722f6d9c9be4ba762e761a72b194
stephenpaul-03 <stephenpaul4040@gmail.com>
2025-11-26 23:04:19 +0530

    refactor: split monolithic CLI file into modular structure

--- bin/index.js
+++ bin/index.js
@@ -1,19 +1,16 @@
 #!/usr/bin/env node
-import { fileURLToPath } from "url";
-import path from "path";
-
 const [, , command, ...args] = process.argv;
 
 async function main() {
   switch (command) {
     case "ignite": {
-      const { default: ignite } = await import("../lib/ignite.js");
-      await ignite(args[0], args.slice(1)); // Pass remaining args as flags
+      const { default: ignite } = await import("../lib/ignite/index.js");
+      await ignite(args[0], args.slice(1)); 
       break;
     }
     default:
       console.log(`Unknown command: ${command}`);
-      console.log("Available commands: spark, ignite");
+      console.log("Available command: ignite");
   }
 }
 
